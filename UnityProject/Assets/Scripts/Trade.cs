a4407ac2ad9be6dae36f84729b76aef11d9d5e87
drgvszi <33577111+drgvszi@users.noreply.github.com>
2020-05-10 22:47:04 +0300

    ENDTURN+Avatar+Trade+etc
    ..

---
+++ UnityProject/Assets/Scripts/Trade.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trade : MonoBehaviour
+{
+    public GameObject block;
+    public GameObject makeitGONE;
+    public bool ok = false;
+    public void ShowOrHide()
+    {
+        if (ok == false)
+            Show();
+        else
+            Hide();
+    }
+    public void Show()
+    {
+        makeitGONE.SetActive(false);
+        block.SetActive(true);
+        ok = true;
+    }
+    public void Hide()
+    {
+        makeitGONE.SetActive(true);
+        block.SetActive(false);
+        ok = false;
+    }
+
+
+}
