b64706941e284210198ad4999b497d461876aa2f
robert216434 <robertmartonca@yahoo.com>
2020-05-11 10:10:33 +0300

    Adaugare gamemap settings pop-up menu

---
+++ UnityProject/Assets/Scripts/GameMapSettings.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMapSettings : MonoBehaviour
+{
+    public GameObject settings;
+
+    public void showSettings()
+    {
+        settings.transform.gameObject.SetActive(true);
+    }
+
+    public void hideSettings()
+    {
+        settings.transform.gameObject.SetActive(false);
+    }
+}
