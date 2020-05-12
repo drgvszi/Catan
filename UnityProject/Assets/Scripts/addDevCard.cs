6be32fa7ec4a0e61cac420bb8ccdc1d1aca894fe
TheodoD <61494882+TheodoD@users.noreply.github.com>
2020-05-12 23:24:35 +0300

    Development Cards

---
+++ UnityProject/Assets/Scripts/addDevCard.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class addDevCard : MonoBehaviour
+{
+    public Text text;
+    public int counter;
+
+
+    public void countPP()
+    {
+        counter = counter + 1;
+    }
+
+    void Update ()
+    {
+        text.text = counter.ToString();
+    }
+
+}
