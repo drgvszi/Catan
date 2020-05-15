426505e267acf84e362f4010fc3f3f9ef7b16e82
drgvszi <33577111+drgvszi@users.noreply.github.com>
2020-05-15 05:05:13 +0300

    bug fix
    end turn, buildcosts, etc

--- UnityProject/Assets/Scripts/BlockButt.cs
+++ UnityProject/Assets/Scripts/BlockButt.cs
@@ -8,6 +8,8 @@ public class BlockButt : MonoBehaviour
     public GameObject makeitGONE;
     public GameObject makeitGONE1;
     public GameObject makeitGONE2;
+    public GameObject makeitGONE3;
+    //public GameObject dices;
     public bool ok = false;
     public void ShowOrHide()
     {
@@ -21,6 +23,7 @@ public class BlockButt : MonoBehaviour
         makeitGONE.SetActive(false);
         makeitGONE1.SetActive(false);
         makeitGONE2.SetActive(false);
+        makeitGONE3.SetActive(true);
         block.SetActive(true);
         ok = true;
     }
@@ -29,6 +32,7 @@ public class BlockButt : MonoBehaviour
         makeitGONE.SetActive(true);
         makeitGONE1.SetActive(true);
         makeitGONE2.SetActive(true);
+        makeitGONE3.SetActive(false);
         block.SetActive(false);
         ok = false;
     }
