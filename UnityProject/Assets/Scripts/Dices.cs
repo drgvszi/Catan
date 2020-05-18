3d36576f89c0fbf9833b84b40360bf8397bb6d3d
TeodorMiron <48171677+cmiron23@users.noreply.github.com>
2020-05-18 22:08:48 +0200

    Dices , place ports

---
+++ UnityProject/Assets/Scripts/Dices.cs
@@ -0,0 +1,171 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine.UI;
+using UnityEditor;
+using UnityEngine;
+using FullSerializer;
+using Proyecto26;
+using UnityEngine.SceneManagement;
+using System.IO;
+using System.Text;
+public class Dices : MonoBehaviour
+{
+    public GameObject side1;
+    public GameObject side2;
+    public GameObject side3;
+    public GameObject side4;
+    public GameObject side5;
+    public GameObject side6;
+    public GameObject sside1;
+    public GameObject sside2;
+    public GameObject sside3;
+    public GameObject sside4;
+    public GameObject sside5;
+    public GameObject sside6;
+
+     public void Show()
+    {
+        int j,i;
+        Text txt = FindTextFiel.find();
+        MakeRequestResponse command = new MakeRequestResponse();
+        command.gameId = LoginScript.CurrentUserGameId;
+        command.playerId = LoginScript.CurrentUserGEId;
+        RequestJson req = new RequestJson();
+        RestClient.Post<RequestJson>("https://catan-connectivity.herokuapp.com/game/rollDice", command).Then(Response =>
+        {
+            req.code = Response.code;
+            req.status = Response.status;
+            Debug.Log(Response.code);
+            Debug.Log(Response.status);
+
+            j = int.Parse(req.arguments[0]);
+            i = int.Parse(req.arguments[1]);
+            switch (j)
+            {
+                case 1:
+                    side1.SetActive(true);
+                    side2.SetActive(false);
+                    side3.SetActive(false);
+                    side4.SetActive(false);
+                    side5.SetActive(false);
+                    side6.SetActive(false);
+
+                    break;
+
+                case 2:
+                    side1.SetActive(false);
+                    side2.SetActive(true);
+                    side3.SetActive(false);
+                    side4.SetActive(false);
+                    side5.SetActive(false);
+                    side6.SetActive(false);
+
+                    break;
+                case 3:
+                    side1.SetActive(false);
+                    side2.SetActive(false);
+                    side3.SetActive(true);
+                    side4.SetActive(false);
+                    side5.SetActive(false);
+                    side6.SetActive(false);
+
+                    break;
+
+                case 4:
+                    side1.SetActive(false);
+                    side2.SetActive(false);
+                    side3.SetActive(false);
+                    side4.SetActive(true);
+                    side5.SetActive(false);
+                    side6.SetActive(false);
+
+                    break;
+                case 5:
+                    side1.SetActive(false);
+                    side2.SetActive(false);
+                    side3.SetActive(false);
+                    side4.SetActive(false);
+                    side5.SetActive(true);
+                    side6.SetActive(false);
+
+                    break;
+
+                case 6:
+                    side1.SetActive(false);
+                    side2.SetActive(false);
+                    side3.SetActive(false);
+                    side4.SetActive(false);
+                    side5.SetActive(false);
+                    side6.SetActive(true);
+
+                    break;
+            }
+
+            switch (i)
+            {
+                case 1:
+                    sside1.SetActive(true);
+                    sside2.SetActive(false);
+                    sside3.SetActive(false);
+                    sside4.SetActive(false);
+                    sside5.SetActive(false);
+                    sside6.SetActive(false);
+
+                    break;
+
+                case 2:
+                    sside1.SetActive(false);
+                    sside2.SetActive(true);
+                    sside3.SetActive(false);
+                    sside4.SetActive(false);
+                    sside5.SetActive(false);
+                    sside6.SetActive(false);
+                    break;
+                case 3:
+                    sside1.SetActive(false);
+                    sside2.SetActive(false);
+                    sside3.SetActive(true);
+                    sside4.SetActive(false);
+                    sside5.SetActive(false);
+                    sside6.SetActive(false);
+                    break;
+
+                case 4:
+                    sside1.SetActive(false);
+                    sside2.SetActive(false);
+                    sside3.SetActive(false);
+                    sside4.SetActive(true);
+                    sside5.SetActive(false);
+                   sside6.SetActive(false);
+                    break;
+                case 5:
+                    sside1.SetActive(false);
+                    sside2.SetActive(false);
+                    sside3.SetActive(false);
+                    sside4.SetActive(false);
+                    sside5.SetActive(true);
+                    sside6.SetActive(false);
+                    break;
+
+                case 6:
+                    sside1.SetActive(false);
+                    sside2.SetActive(false);
+                    sside3.SetActive(false);
+                    sside4.SetActive(false);
+                    sside5.SetActive(false);
+                    sside6.SetActive(true);
+                    break;
+            }
+            txt.text = req.status;
+        }).Catch(err => { Debug.Log(err); });
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ UnityProject/Assets/Scripts/PlacePort.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlacePort : MonoBehaviour
+{
+    public Transform Spawn;
+    public GameObject port1;
+    public GameObject port2;
+    public GameObject port3;
+    public GameObject port4;
+    public GameObject port5;
+    BoardConnectivityJson board = null;
+    public int nr;
+    bool done = false;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        BoardConnectivityJson board = ReceiveBoardScript.ReceivedBoard;
+        if (done == false)
+        {
+            if (ReceiveBoardScript.ReceivedBoard.board[0] != null)
+            {
+                done = true;
+                string str = "";
+                str = board.ports[nr];
+               
+                switch (str)
+                {
+                    case "ThreeForOne":
+                        Instantiate(port1, Spawn.position, Spawn.rotation);
+                        break;
+                    case "Brick":
+                        Instantiate(port2, Spawn.position, Spawn.rotation);
+                        break;
+                    case "Ore":
+                        Instantiate(port3, Spawn.position, Spawn.rotation);
+                        break;
+                    case "Wool":
+                        Instantiate(port4, Spawn.position, Spawn.rotation);
+                        break;
+                    case "Lumber":
+                        Instantiate(port5, Spawn.position, Spawn.rotation);
+                        break;
+                    case "Grain":
+                        Instantiate(port5, Spawn.position, Spawn.rotation);
+                        break;
+
+
+                }
+            }
+            // else
+            //     Debug.Log("Null");
+        }
+    }
+}
