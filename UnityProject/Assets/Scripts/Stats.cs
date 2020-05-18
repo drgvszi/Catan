5c47596664fde6bd041fb61ad04f356248dba5d5
drgvszi <33577111+drgvszi@users.noreply.github.com>
2020-05-18 17:04:43 +0300

    Integrare

---
+++ UnityProject/Assets/Scripts/Stats.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stats : MonoBehaviour
+{
+    public GameObject makeitGONE;
+    public GameObject makeitGONE1;
+    public void Show()
+    {
+        makeitGONE.SetActive(true);
+        makeitGONE1.SetActive(false);
+    }
+
+
+}
---
+++ UnityProject/Assets/Scripts/Stats1.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stats1 : MonoBehaviour
+{
+    public GameObject makeitGONE;
+    public GameObject makeitGONE1;
+
+    public void Show()
+    {
+        makeitGONE.SetActive(true);
+        makeitGONE1.SetActive(false);
+        
+    }
+   
+
+
+}
