8f8d5096facf0cbea2fd8bcdce8f5ef22b853a3d
robert216434 <robertmartonca@yahoo.com>
2020-05-19 17:06:21 +0300

    Modificari memorare setari

--- UnityProject/Assets/Scripts/mainMenu.cs
+++ UnityProject/Assets/Scripts/mainMenu.cs
@@ -8,6 +8,53 @@ using System.Linq;
 
 public class mainMenu : MonoBehaviour
 {
+
+    Resolution[] resolutions;
+    public Dropdown resolutionDropdown;
+    int currentResolutionIndex = 0;
+
+    public Slider volumeSlider;
+    public Slider musicSlider;
+    public Dropdown qualityDropdown;
+
+    private void Start()
+    {
+
+        //setari memorate
+        AudioListener.volume = PlayerPrefs.GetFloat("volum");
+        volumeSlider.value = PlayerPrefs.GetFloat("volum");
+        audioMixer.SetFloat("volume", PlayerPrefs.GetFloat("volum"));
+        music.volume = PlayerPrefs.GetFloat("music");
+        musicSlider.value = PlayerPrefs.GetFloat("music");
+        QualitySettings.SetQualityLevel(PlayerPrefs.GetInt("qualityIndex"));
+        qualityDropdown.value= PlayerPrefs.GetInt("qualityIndex");
+        Screen.fullScreen = PlayerPrefs.GetInt("fullScreen") == 1 ? true : false;
+        Resolution res = resolutions[PlayerPrefs.GetInt("resolution")];
+        Screen.SetResolution(res.width, res.height, Screen.fullScreen);
+
+
+        resolutions = Screen.resolutions.Select(resolution => new Resolution { width = resolution.width, height = resolution.height }).Distinct().ToArray();
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+        Debug.Log(resolutions.Length);
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + "x" + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
     public AudioMixer audioMixer;
     public void SetVolume (float volume)
     {
@@ -25,51 +72,15 @@ public class mainMenu : MonoBehaviour
 
     public void SetQuality (int qualityIndex)
     {
+        PlayerPrefs.SetFloat("qualityIndex", qualityIndex);
         QualitySettings.SetQualityLevel(qualityIndex);
-        PlayerPrefs.SetFloat("quality", qualityIndex);
     }
 
     public void SetFullscreen (bool isFullscreen)
     {
         Screen.fullScreen = isFullscreen;
         PlayerPrefs.SetInt("fullScreen", isFullscreen ? 1 : 0);
-    }
 
-    Resolution[] resolutions;
-    public Dropdown resolutionDropdown;
-    int currentResolutionIndex = 0;
-    private void Start()
-    {
-        //setari memorate
-        AudioListener.volume = PlayerPrefs.GetFloat("volum");
-        audioMixer.SetFloat("volume", PlayerPrefs.GetFloat("volum"));
-        music.volume = PlayerPrefs.GetFloat("music");
-        QualitySettings.SetQualityLevel(PlayerPrefs.GetInt("quality"));
-        Screen.fullScreen = PlayerPrefs.GetInt("fullScreen") == 1 ? true : false;
-        Resolution res = resolutions[PlayerPrefs.GetInt("resolution")];
-        Screen.SetResolution(res.width, res.height, Screen.fullScreen);
-
-
-        resolutions = Screen.resolutions.Select(resolution => new Resolution { width = resolution.width, height = resolution.height }).Distinct().ToArray();
-
-        resolutionDropdown.ClearOptions();
-
-        List<string> options = new List<string>();
-        Debug.Log(resolutions.Length);
-        for(int i = 0; i < resolutions.Length; i++)
-        {
-            string option = resolutions[i].width + "x" + resolutions[i].height;
-            options.Add(option);
-
-            if(resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
-            {
-                currentResolutionIndex = i;
-            }
-        }
-
-        resolutionDropdown.AddOptions(options);
-        resolutionDropdown.value = currentResolutionIndex;
-        resolutionDropdown.RefreshShownValue();
     }
 
     public void SetResolution (int resolutionIndex)
