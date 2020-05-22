edc09f0c4c4e680f27ff1c8bd9e6ac8c6d47e9dc
Theodor David <theodor.david18@gmail.com>
2020-05-22 10:44:52 +0300

    Development Cards pop-ups

--- UnityProject/Assets/Scripts/developAction.cs
+++ UnityProject/Assets/Scripts/developAction.cs
@@ -1,12 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Runtime.InteropServices;
 using System.Security.Cryptography;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class developAction : MonoBehaviour
 {
-    public GameObject card;
+    public GameObject cardPopUp;
+    public GameObject noPopUp;
+    public Text txt;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,6 +24,10 @@ public class developAction : MonoBehaviour
     }
     public void asd1()
     {
-        print(card.name);
+        print(txt.text);
+        if (txt.text != "0")
+            cardPopUp.SetActive(true);
+        else
+            noPopUp.SetActive(true); 
     }
 }
