e2a9e29c9d33db1c5d11512b47afab5a90013890
TeodorMiron <48171677+cmiron23@users.noreply.github.com>
2020-05-25 11:44:18 +0200

    bug test

--- UnityProject/Assets/Scripts/SocketIoscript.cs
+++ UnityProject/Assets/Scripts/SocketIoscript.cs
@@ -495,18 +495,24 @@ public class SocketIoscript : MonoBehaviour
                     player2.SetActive(false);
                 }
             }
-            MakeRequestResponse command = new MakeRequestResponse();
-            command.gameId = LoginScript.CurrentUserGameId;
-            command.playerId = LoginScript.CurrentUserGEId;
-            RequestJson req = new RequestJson();
-            RestClient.Post<UpdateJson>("https://catan-connectivity.herokuapp.com/game/update", command).Then(Response =>
+            MakeRequestResponse command1 = new MakeRequestResponse();
+            command1.gameId = LoginScript.CurrentUserGameId;
+            command1.playerId = LoginScript.CurrentUserGEId;
+            RequestJson req1 = new RequestJson();
+            RestClient.Post<UpdateJson>("https://catan-connectivity.herokuapp.com/game/update", command1).Then(Response1 =>
             {
-                Debug.Log("Update code " + Response.code);
-                Debug.Log("Update status " + Response.status);
-                Debug.Log("Update arguments lumber " + Response.arguments.lumber);
-                Debug.Log("Update arguments settle " + Response.arguments.settlements[1]);
+                Debug.Log("Update code " + Response1.code);
+                Debug.Log("Update status " + Response1.status);
+                Debug.Log("Update arguments lumber " + Response1.arguments.lumber);
+                Debug.Log("Update arguments settle " + Response1.arguments.settlements[1]);
                 // Debug.Log("Update roads " + Response.arguments.roads[0][1]);//NU MERGE ASTA
                 // Debug.Log("Update roads " + Response.arguments.roads[0][0]);//NU MERGE NICI ASTA
+
+                lumber.text = Response1.arguments.lumber.ToString();
+                ore.text = Response1.arguments.ore.ToString();
+                grain.text = Response1.arguments.grain.ToString();
+                brick.text = Response1.arguments.brick.ToString();
+                wool.text = Response1.arguments.wool.ToString();
             }).Catch(err => { Debug.Log(err); });
 
         });
