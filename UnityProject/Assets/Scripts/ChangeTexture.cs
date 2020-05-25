80589cd2314a3398d2545be47276dffa4c21ed12
Theodor David <theodor.david18@gmail.com>
2020-05-25 17:57:16 +0300

    same bug changed

--- UnityProject/Assets/Scripts/ChangeTexture.cs
+++ UnityProject/Assets/Scripts/ChangeTexture.cs
@@ -25,6 +25,10 @@ public class ChangeTexture : MonoBehaviour
 
     // Start is called before the first frame update
  
+    void Start()
+    {
+        done = false;
+    }
 
     // Update is called once per frame
     void Update()
